cd1cf87aeef9fafe2a6524195d757e15348a7568
Jay Morrington <animating.du.38@gmail.com>
2024-11-19 16:17:12 +0100

    Completly responsive image display (i want to die but it's working)

--- OpenImage.cpp
+++ OpenImage.cpp
@@ -1,251 +1,201 @@
-// OpenImage.cpp : Définit le point d'entrée de l'application.
-//
+// OpenImage.cpp : Defines the entry point for the application.
 
 #include <Windows.h>
 #include <objidl.h>
 #include <gdiplus.h>
 #include <string>
-#include <iostream>
 
 #include "framework.h"
 #include "OpenImage.h"
 
 #define MAX_LOADSTRING 100
 
+#pragma comment (lib, "Gdiplus.lib")
+
 using namespace Gdiplus;
 using namespace std;
 
-#pragma comment (lib,"Gdiplus.lib")
-
 wstring current_path;
 
-VOID OnPaint(HDC hdc)
-{
+// Global Variables
+HINSTANCE hInst;
+const int defaultWidth = 960;
+const int defaultHeight = 540;
+WCHAR szTitle[MAX_LOADSTRING];
+WCHAR szWindowClass[MAX_LOADSTRING];
+
+// Function Prototypes
+ATOM RegisterAppClass(HINSTANCE hInstance);
+BOOL InitializeApp(HINSTANCE hInstance, int nCmdShow);
+LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
+INT_PTR CALLBACK AboutDialogProc(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam);
+
+// Helper: Handle painting the window
+void DrawImageInBox(HDC hdc, int boxX, int boxY, int boxWidth, int boxHeight) {
+    // Initialize GDI+ graphics
     Graphics graphics(hdc);
+
+    // Load the image
     Image image(current_path.c_str());
-    Pen pen(Color(255, 0, 0, 0));
-    graphics.DrawRectangle(&pen, Rect(0, 0, 800, 400));
-    graphics.DrawImage(&image, Rect(0, 0, 800, 400));
 
-}
+    // Get the original dimensions of the image
+    UINT imgWidth = image.GetWidth();
+    UINT imgHeight = image.GetHeight();
 
-// Variables globales :
-HINSTANCE hInst;                                // instance actuelle
-WCHAR szTitle[MAX_LOADSTRING];                  // Texte de la barre de titre
-WCHAR szWindowClass[MAX_LOADSTRING];            // nom de la classe de fenêtre principale
+    // Calculate the aspect ratios
+    float imgAspectRatio = static_cast<float>(imgWidth) / imgHeight;
+    float boxAspectRatio = static_cast<float>(boxWidth) / boxHeight;
 
-
-// Déclarations anticipées des fonctions incluses dans ce module de code :
-ATOM                MyRegisterClass(HINSTANCE hInstance);
-BOOL                InitInstance(HINSTANCE, int);
-LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
-INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
-
-int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
-                     _In_opt_ HINSTANCE hPrevInstance,
-                     _In_ LPWSTR    lpCmdLine,
-                     _In_ int       nCmdShow)
-{
-    UNREFERENCED_PARAMETER(hPrevInstance);
-    UNREFERENCED_PARAMETER(lpCmdLine);
-
-    GdiplusStartupInput gdiplusStartupInput;
-    ULONG_PTR           gdiplusToken;
-
-    GdiplusStartup(&gdiplusToken, &gdiplusStartupInput, NULL);
-
-    // TODO: Placez le code ici.
-
-    // Initialise les chaînes globales
-    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
-    LoadStringW(hInstance, IDC_OPENIMAGE, szWindowClass, MAX_LOADSTRING);
-    MyRegisterClass(hInstance);
-
-    // Effectue l'initialisation de l'application :
-    if (!InitInstance (hInstance, nCmdShow))
-    {
-        return FALSE;
+    // Determine the final dimensions of the image within the box
+    int drawWidth, drawHeight, offsetX, offsetY;
+    if (imgAspectRatio > boxAspectRatio) {
+        // Image is wider than the box
+        drawWidth = boxWidth;
+        drawHeight = static_cast<int>(boxWidth / imgAspectRatio);
+        offsetX = boxX;
+        offsetY = boxY + (boxHeight - drawHeight) / 2; // Center vertically
+    }
+    else {
+        // Image is taller than or fits the box
+        drawWidth = static_cast<int>(boxHeight * imgAspectRatio);
+        drawHeight = boxHeight;
+        offsetX = boxX + (boxWidth - drawWidth) / 2; // Center horizontally
+        offsetY = boxY;
     }
 
-    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_OPENIMAGE));
+    // Draw the responsive box
+    Pen pen(Color(255, 0, 0, 0)); // Black border
+    graphics.DrawRectangle(&pen, Rect(boxX, boxY, boxWidth, boxHeight));
 
+    // Draw the image within the calculated bounds
+    graphics.DrawImage(&image, Rect(offsetX, offsetY, drawWidth, drawHeight));
+}
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE, _In_ LPWSTR, _In_ int nCmdShow) {
+    GdiplusStartupInput gdiplusStartupInput;
+    ULONG_PTR gdiplusToken;
+    GdiplusStartup(&gdiplusToken, &gdiplusStartupInput, nullptr);
+
+    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+    LoadStringW(hInstance, IDC_OPENIMAGE, szWindowClass, MAX_LOADSTRING);
+    RegisterAppClass(hInstance);
+
+    if (!InitializeApp(hInstance, nCmdShow)) return FALSE;
+
+    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_OPENIMAGE));
     MSG msg;
 
-    // Boucle de messages principale :
-    while (GetMessage(&msg, nullptr, 0, 0))
-    {
-        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
-        {
+    while (GetMessage(&msg, nullptr, 0, 0)) {
+        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg)) {
             TranslateMessage(&msg);
             DispatchMessage(&msg);
         }
     }
 
     GdiplusShutdown(gdiplusToken);
-    return (int) msg.wParam;
+    return (int)msg.wParam;
 }
 
-//
-//  FONCTION : MyRegisterClass()
-//
-//  OBJECTIF : Inscrit la classe de fenêtre.
-//
-ATOM MyRegisterClass(HINSTANCE hInstance)
-{
-    WNDCLASSEXW wcex;
-
-    wcex.cbSize = sizeof(WNDCLASSEX);
-
-    wcex.style          = CS_HREDRAW | CS_VREDRAW;
-    wcex.lpfnWndProc    = WndProc;
-    wcex.cbClsExtra     = 0;
-    wcex.cbWndExtra     = 0;
-    wcex.hInstance      = hInstance;
-    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_OPENIMAGE));
-    wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
-    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
-    wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_OPENIMAGE);
-    wcex.lpszClassName  = szWindowClass;
-    wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
-
+ATOM RegisterAppClass(HINSTANCE hInstance) {
+    WNDCLASSEXW wcex = { sizeof(WNDCLASSEX) };
+    wcex.style = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc = WindowProc;
+    wcex.hInstance = hInstance;
+    wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_OPENIMAGE));
+    wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
+    wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+    wcex.lpszMenuName = MAKEINTRESOURCEW(IDC_OPENIMAGE);
+    wcex.lpszClassName = szWindowClass;
+    wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
     return RegisterClassExW(&wcex);
 }
 
-//
-//   FONCTION : InitInstance(HINSTANCE, int)
-//
-//   OBJECTIF : enregistre le handle d'instance et crée une fenêtre principale
-//
-//   COMMENTAIRES :
-//
-//        Dans cette fonction, nous enregistrons le handle de l'instance dans une variable globale, puis
-//        nous créons et affichons la fenêtre principale du programme.
-//
-BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
-{
-   hInst = hInstance; // Stocke le handle d'instance dans la variable globale
+BOOL InitializeApp(HINSTANCE hInstance, int nCmdShow) {
+    hInst = hInstance;
 
-   HWND hWnd = CreateWindowW(szWindowClass, szTitle, (WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_MINIMIZEBOX),
-      0, 0, 800, 600, nullptr, nullptr, hInstance, nullptr);
+    HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+        0, 0, defaultWidth, defaultHeight, nullptr, nullptr, hInstance, nullptr);
 
-   if (!hWnd)
-   {
-      return FALSE;
-   }
+    if (!hWnd) return FALSE;
 
-   //CreateWindow(TEXT("button"), TEXT("Beep"),
-   //    WS_VISIBLE | WS_CHILD,
-   //    20, 50, 80, 25,
-   //    hWnd, (HMENU)1, NULL, NULL);
-
-   HWND trackbarHWND = CreateWindowW(TEXT("Trackbar"), L"Trackbar",
-       WS_OVERLAPPEDWINDOW | WS_VISIBLE, 100, 100, 350, 180, 0, 0, hInstance, 0);
-
-   ShowWindow(hWnd, nCmdShow);
-   UpdateWindow(hWnd);
-
-   return TRUE;
+    ShowWindow(hWnd, nCmdShow);
+    UpdateWindow(hWnd);
+    return TRUE;
 }
 
-//
-//  FONCTION : WndProc(HWND, UINT, WPARAM, LPARAM)
-//
-//  OBJECTIF : Traite les messages pour la fenêtre principale.
-//
-//  WM_COMMAND  - traite le menu de l'application
-//  WM_PAINT    - Dessine la fenêtre principale
-//  WM_DESTROY  - génère un message d'arrêt et retourne
-//
-//
-LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
-{
-    switch (message)
-    {
-    case WM_COMMAND:
-        {
-            int wmId = LOWORD(wParam);
-            // Analyse les sélections de menu :
-            switch (wmId)
-            {
-            case ID_MORE_ABOUT:
-                DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
-                break;
-            case IDM_FILES_EXIT:
-                DestroyWindow(hWnd);
-                break;
-            case IDM_FILES_OPEN:
-            {
-                wstring path(MAX_PATH, '\0');   // buffer for file name MAX_PATH = 260
-                OPENFILENAME ofn = { sizeof(OPENFILENAME) };  // common dialog box structure
+LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
+    switch (message) {
+    case WM_COMMAND: {
+        int wmId = LOWORD(wParam);
+        switch (wmId) {
+        case ID_MORE_ABOUT: {
+            DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, AboutDialogProc);
+            return 0;
+        }
+        case IDM_FILES_EXIT: {
+            DestroyWindow(hWnd);
+            return 0;
+        }
+        case IDM_FILES_OPEN: {
+            OPENFILENAME ofn = { sizeof(OPENFILENAME) };
+            wstring path(MAX_PATH, '\0');
 
-                // Initialize OPENFILENAME
-                ZeroMemory(&ofn, sizeof(ofn));
-                ofn.lStructSize = sizeof(ofn);
-                ofn.hwndOwner = hWnd;
-                ofn.lpstrFile = &path[0];
-                ofn.nMaxFile = MAX_PATH;
-                ofn.lpstrFilter = TEXT("All Images\0*.BMP;*.JPG;*.JPEG;*.PNG;*.GIF;*.TIFF\0JPEG Files\0*.JPG;*.JPEG\0PNG Files\0*.PNG\0GIF Files\0*.GIF\0BMP Files\0*.BMP\0TIFF Files\0*.TIFF\0All Files\0*.*\0");
-                ofn.nFilterIndex = 1;
-                ofn.lpstrFileTitle = NULL;
-                ofn.nMaxFileTitle = 0;
-                ofn.lpstrInitialDir = NULL;
-                ofn.Flags = OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST | OFN_NOCHANGEDIR;
+            ofn.hwndOwner = hWnd;
+            ofn.lpstrFile = &path[0];
+            ofn.nMaxFile = MAX_PATH;
+            ofn.lpstrFilter = TEXT("All Images\0*.BMP;*.JPG;*.PNG;*.GIF;*.TIFF\0All Files\0*.*\0");
+            ofn.Flags = OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST;
 
-                if (GetOpenFileName(&ofn))
-                {
-                    // Get the actual string size, excluding the null-terminator.
-                    size_t actualLength = wcslen(path.c_str());
-
-                    // Resize the wstring to the correct size (length of the path, excluding null terminator)
-                    path.resize(actualLength);
-                }
-
+            if (GetOpenFileName(&ofn)) {
+                path.resize(wcslen(path.c_str()));
                 current_path = path;
-
-                RedrawWindow(hWnd, NULL, NULL, RDW_INVALIDATE);
-
-                break;
+                RedrawWindow(hWnd, nullptr, nullptr, RDW_INVALIDATE);
             }
-            default:
-                return DefWindowProc(hWnd, message, wParam, lParam);
-            }
+            return 0;
         }
-        break;
-    case WM_PAINT:
-        {
-            PAINTSTRUCT ps;
-            HDC hdc = BeginPaint(hWnd, &ps);
-
-            OnPaint(hdc);
-
-            EndPaint(hWnd, &ps);
+        default: {
+            return DefWindowProc(hWnd, message, wParam, lParam);
         }
-        break;
+        }
+        return 0;
+    }
+    case WM_PAINT: {
+        PAINTSTRUCT ps;
+        HDC hdc = BeginPaint(hWnd, &ps);
+
+        // Get the dimensions of the client area
+        RECT clientRect;
+        GetClientRect(hWnd, &clientRect);
+        int windowWidth = clientRect.right - clientRect.left;
+        int windowHeight = clientRect.bottom - clientRect.top;
+
+        // Define the box size and position (example: centered box taking 50% width and 40% height)
+        int boxWidth = static_cast<int>(windowWidth * 0.5);
+        int boxHeight = static_cast<int>(windowHeight * 0.90);
+        int boxX = (windowWidth - boxWidth) / 16; // Center horizontally
+        int boxY = (windowHeight - boxHeight) / 2; // Center vertically
+
+        // Call the function with the calculated box dimensions
+        DrawImageInBox(hdc, boxX, boxY, boxWidth, boxHeight);
+
+        EndPaint(hWnd, &ps);
+        return 0;
+    }
     case WM_DESTROY:
         PostQuitMessage(0);
-        break;
+        return 0;
     default:
         return DefWindowProc(hWnd, message, wParam, lParam);
     }
-    return 0;
+    return DefWindowProc(hWnd, message, wParam, lParam);
 }
 
-// Gestionnaire de messages pour la boîte de dialogue À propos de.
-INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
-{
+INT_PTR CALLBACK AboutDialogProc(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam) {
     UNREFERENCED_PARAMETER(lParam);
-    switch (message)
-    {
-    case WM_INITDIALOG:
+    if (message == WM_INITDIALOG) return (INT_PTR)TRUE;
+    if (message == WM_COMMAND && (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)) {
+        EndDialog(hDlg, LOWORD(wParam));
         return (INT_PTR)TRUE;
-
-    case WM_COMMAND:
-        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
-        {
-            EndDialog(hDlg, LOWORD(wParam));
-            return (INT_PTR)TRUE;
-        }
-        break;
     }
     return (INT_PTR)FALSE;
 }
